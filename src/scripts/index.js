6f119e8b1f09faeec0212f50670673474ed63e94
Megana <megana15081990@yandex.ru>
2024-07-11 15:14:12 +0700

    попап в картинкой(еще не готов)

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -16,6 +16,8 @@ function createCard(card, deleteCard, openPopupImg) {
   cardElement.querySelector('.card__image').alt = card.name;
   cardElement.querySelector('.card__title').textContent = card.name;
   deleteButton.addEventListener('click', deleteCard);
+
+
   return cardElement;
 }
 
@@ -29,7 +31,7 @@ function deleteCard(evt) {
 // @todo: Вывести карточки на страницу
 
 initialCards.forEach((item) => {
-  placesList.append(createCard(item, deleteCard));
+  placesList.append(createCard(item, deleteCard, openPopupImg));
 });
 
 
@@ -40,12 +42,14 @@ const profileAddButton = document.querySelector ('.profile__add-button')
 const newCardPopup = document.querySelector ('.popup_type_new-card')
 const closeNewCArdPopupButton = newCardPopup.querySelector ('.popup__close');
 const imgPopup = document.querySelector ('.popup_type_image')
-const openImage = document.querySelector('.popup__image');
-const openImageName = document.querySelector('.popup__caption')
+const openImage = imgPopup.querySelector('.popup__image');
+const openImageCaption = imgPopup.querySelector('.popup__caption')
 
 //окртыие popup
 function openPopup(popupElement) {
   popupElement.classList.add('popup_is-opened');
+  document.addEventListener('keyup', popupCloseEsc)
+  document.addEventListener('mouseup', popupCloseOverlay)
 }
 
 editPopupButton.addEventListener('click', function () {
@@ -57,18 +61,21 @@ profileAddButton.addEventListener('click', function() {
   openPopup(newCardPopup)
 });
 
+// открытие картинки
 
-  function openPopupImg({link, name}) {
 
-  openImage.src = link;
-  openImageName.textContent = name;
-  openImageName.alt = name;
-  openPopup(imgPopup)
-};
+function openPopupImg(item) {
+  openImage.src = item.link;
+  openImage.alt = item.name;
+  openImageCaption.textContent = item.name;
+  openPopup(imgPopup);
+}
 
 // //закрытие popup
 function closePopup(popupElement) {
   popupElement.classList.remove('popup_is-opened');
+  document.removeEventListener('keyup', popupCloseEsc)
+  document.removeEventListener('mouseup', popupCloseOverlay)
 }
 
 closePopupButton.addEventListener('click', function() {
@@ -81,8 +88,48 @@ closeNewCArdPopupButton.addEventListener('click', function() {
 
 });
 
+// закртиые кнопкой Esc
+function popupCloseEsc (evt) {
+  if (evt.key === 'Escape') {
+    const openedPopup = document.querySelector('.popup_is-opened');
+closePopup(openedPopup)
+  }
+}
+
+// закрытие на оверлей
+function popupCloseOverlay (evt) {
+  if (evt.target.classList.contains('popup_is-opened')) {
+    const openedPopup = document.querySelector('.popup_is-opened');
+    closePopup(openedPopup);
+}
+}
 
 
 
 
 
+
+// Редактирование имени и информации о себе
+// Находим форму в DOM
+const formElement = document.querySelector('.popup__form')
+// Находим поля формы в DOM
+const nameInput = formElement.elements.name;
+const jobInput = formElement.querySelector('.popup__input_type_description')
+
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
+function handleFormSubmit(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+                                                // Так мы можем определить свою логику отправки.
+                                                // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+
+    // Выберите элементы, куда должны быть вставлены значения полей
+
+    // Вставьте новые значения с помощью textContent
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', handleFormSubmit); 
