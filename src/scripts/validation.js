c3ae2e74afbf6a3865cfbcfb40c61bdf3d7d2291
Megana <megana15081990@yandex.ru>
2024-07-30 21:51:55 +0700

    валидация формы_3

--- src/scripts/validation.js
+++ src/scripts/validation.js
@@ -9,19 +9,19 @@ const  validationConfig = {
 }
   
 
-//валидация формы "редактрировать профиль"
+//валидация формы 
 
 const showInputError = (formElement, inputElement, errorMessage) => {
     const errorElement = formElement.querySelector(`.${inputElement.id}-error`)
    inputElement.classList.add('popup__input_type_error');
    errorElement.textContent = errorMessage;
-   errorElement.classList.add('popup__error_visible');
+   errorElement.classList.add('popup__input-error_active');
   };
 
   const hideInputError = (formElement, inputElement) => {
     const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
     inputElement.classList.remove('popup__input_type_error');
-    errorElement.classList.remove('popup__error_visible');
+    errorElement.classList.remove('popup__input-error_active');
     errorElement.textContent = '';
   };
 
@@ -38,5 +38,72 @@ const showInputError = (formElement, inputElement, errorMessage) => {
     hideInputError(formElement, inputElement);
   }
 }; 
-    
+
+
+
+  const checkInputValidity = (formElement, inputElement) => {
+    if (!inputElement.validity.valid) {
+      showInputError(formElement, inputElement, inputElement.validationMessage);
+    } else {
+      hideInputError(formElement, inputElement);
+    }
+  };
+
+  const hasInvalidInput = (inputList) => {
+    return inputList.some((inputElement) => { // возвращайте результат работы метода some для массива inputList.
+      return !inputElement.validity.valid;
+    })
+  };
+
+  const toggleButtonState = ( inputList, buttonElement) => { // функцию, которая отвечает за блокировку кнопки 
+    if(hasInvalidInput(inputList)) {
+      buttonElement.disabled = true;
+  buttonElement.classList.add('popup__button_disabled')
+    } else {
+        buttonElement.disabled = false;
+      buttonElement.classList.remove('popup__button_disabled')
+    }
+  };
+
+  const enableValidation = () => {
+    // Найдём все формы с указанным классом в DOM,
+    // сделаем из них массив методом Array.from
+    const formList = Array.from(document.querySelectorAll('.form'));
+  
+    // Переберём полученную коллекцию
+    formList.forEach((formElement) => {
+      // Для каждой формы вызовем функцию setEventListeners,
+      // передав ей элемент формы
+      formElement.addEventListener('submit', (evt) => {
+        evt.preventDefault();
+      });
+      setEventListeners(formElement);
+    });
+  };
+  
+
+//Добавление обработчиков всем полям формы
+
+const setEventListeners = (formElement) => {
+    const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+    const buttonElement = formElement.querySelector('.popup__button');
+  
+    // чтобы проверить состояние кнопки в самом начале
+    toggleButtonState(inputList, buttonElement);
+  
+    inputList.forEach((inputElement) => {
+      inputElement.addEventListener('input', function () {
+        checkInputValidity(formElement, inputElement);
+        // чтобы проверять его при изменении любого из полей
+        toggleButtonState(inputList, buttonElement);
+      });
+    });
+  }; 
+
+
+  // Вызовем функцию
+  enableValidation(); 
+
+
+//   clearValidation(profileForm, validationConfig); 
   
